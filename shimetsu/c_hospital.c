0f16c289b6427d47b6e56fb7b3647fbeff7b4a9d
U-LAPTOP-ER85SC8R\tyama <tyama@preferred.jp>
2019-01-28 14:25:31 +0900

    Updated.

--- shimetsu/c_hospital.c
+++ shimetsu/c_hospital.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
 #include <string.h>
 
+char *msg_invalid = "Invalid\n";
+
 int main() {
   char t[128];
   int len, i, c_alpha, c_num;
   scanf("%s", t);
   len = strlen(t);
   if (len < 6) {
-    printf("invalid\n");
+    puts(msg_invalid);
     return 0;
   }
   for (i = 0; i < len-2; i++) {
     if (t[i] == t[i+1] && t[i] == t[i+2]) {
-      printf("invalid\n");
+      puts(msg_invalid);
       return 0;
     }
   }
@@ -21,7 +23,7 @@ int main() {
     else if ('a' <= t[i] && t[i] <= 'z') c_alpha = 1;
     else if ('0' <= t[i] && t[i] <= '9') c_num = 1;
   }
-  if (c_alpha && c_num) printf("valid\n");
-  else                  printf("invalid\n");
+  if (c_alpha && c_num) printf("Valid\n");
+  else                  puts(msg_invalid);
   return 0;
 }
