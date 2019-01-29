0e21d2d7b7444e91989eb68afcc5c7da7a2047e4
U-LAPTOP-ER85SC8R\tyama <tyama@preferred.jp>
2019-01-29 18:05:30 +0900

    Updated.

--- shimetsu/b_tower.c
+++ shimetsu/b_tower.c
@@ -10,7 +10,7 @@ void data_read() {
 }
 
 int search_common(int len) {
-  int j, k, kmax, l;
+  int j, k, kmax;
   char str_tmp[128];
   scanf("%s", str_tmp);
   kmax = strlen(str_tmp);
@@ -23,11 +23,11 @@ int search_common(int len) {
     if (str[j] == str_tmp[k]) {
       k++;
     } else {
-      k = 0; l = j+1;
+      k = 0;
     }
   }
-  strcpy(str + l, str_tmp);
-  return l + kmax;
+  strcpy(str + len, str_tmp+k);
+  return len + kmax - k;
 }
 
 void main_loop() {
@@ -36,6 +36,9 @@ void main_loop() {
   for (i = 0; i < n; i++) {
     len = search_common(len);
   }
+  //for (i = 0; i < len+1; i++) {
+  //  printf("%02X ", (int)(str[i]));
+  //}
   return;
 }
 
