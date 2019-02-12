a2758a2fe96ea498d1a32cec2c2765b1c4ebfb9b
U-LAPTOP-ER85SC8R\tyama <tyama@preferred.jp>
2019-02-12 15:47:39 +0900

    Updated.

--- abc117/c.py
+++ abc117/c.py
@@ -3,7 +3,9 @@ a = [int(x) for x in input().split()]
 a.sort()
 b = [a[i+1] - a[i] for i in range(len(a) - 1)]
 b.sort()
-if len(b) <= n-1:
+if n == 1:
+  print(sum(b))
+elif len(b) <= n-1:
   print(0)
 else:
   print(sum(b[0:-n+1]))
