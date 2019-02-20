e9984cb5ffeed04ca30b0b1c677b6066911e36df
U-LAPTOP-ER85SC8R\tyama <tyama@preferred.jp>
2019-02-20 15:23:53 +0900

    Updated.

--- abc116/d.py
+++ abc116/d.py
@@ -1,50 +1,54 @@
+import heapq
+
 n, k = [int(x) for x in input().split()]
 
-data = {}
+data = [[int(x) for x in input().split()] for _ in range(n)]
+data.sort(reverse=True, key=lambda x: x[1])
 
-for _ in range(n):
-  t, d = [int(x) for x in input().split()]
-  if t in data:
-    data[t].append(d)
+eat = {}
+noeat = {}
+
+for x in data[0:k]:
+  if x[0] in eat:
+    heapq.heappush(eat[x[0]], x[1])
   else:
-    data[t] = [d]
+    eat[x[0]] = [x[1]]
 
-for k in data:
-  data[k].sort(reverse=True)
-  for i in range(1, len(data[k])):
-    data[k][i] += data[k][i-1]
-
-ts = [k for k in data]
-ts.sort()
-tmax = len(ts)
-
-init_stack = [[tmax, k, i] for i in range(tmax, 0, -1)]
-stack = init_stack[:]
-memo = {}
-
-while stack:
-  if stack[-1] in memo:
-    stack = stack[0:-1]
-  elif any(x == 0 for x in stack[-1]):
-    memo[stack[-1]] = 0
-    stack = stack[0:-1]
+for x in data[k:]:
+  if x[0] in noeat:
+    heapq.heappush(eat[x[0]], -x[1])
   else:
-    rt, rk, dt = stack[-1] # rest of type, rest of k, duty of type
-    c0 = [[rt-1, i, dt-1] for i in
-          range(dt-1, min(rk, len(data[ts[rt]]))+1)]
-    c1 = [[rt-1, rk, dt-1]]
-    if (not c0) and (not c1):
-      stack = stack[0:-1]
-    elif all(x in memo for x in c0 + c1):
-      a = []
-      for k in c0:
-        a.append(memo[k] + ((rk - data[ts[k[1] - 1]]) if k[1] > 0 else 0))
-      if not c1:
-        a.append(memo[c1[0]])
-      if a:
-        memo[stack[-1]] = max(a)
-      stack = stack[0:-1]
+    eat[x[0]] = [-x[1]]
+
+max_score = 0
+
+while True:
+  score = 0
+  for x in eat:
+    score += sum(eat[x])
+  score += len(eat)**2
+  if max_score < score:
+    max_score = score
+  remove = None
+  min_score1 = 10**12+1
+  for x in eat:
+    if 1 < len(eat[x]) and eat[x][0] < min_score:
+      remove = x
+      min_score1 = eat[x][0]
+  add = None
+  max_score1 = 0
+  for x in noeat:
+    if max_score1 < -noeat[x][0]:
+      add = x
+      max_score1 = -noeat[x][0]
+  if remove == None or add == None:
+    break
+  else:
+    eat[add] = [-max_score1]
+    heapq.heappop(eat[remove])
+    if 1 < len(noeat[add]):
+      heapq.heappop(noeat[add])
     else:
-      stack = stack + c0 + c1
+      noeat.pop(add)
 
-print(max([memo[k] + k[2]**2 for k in init_stack]))
+print(max_score)
