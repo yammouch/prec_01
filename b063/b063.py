c0a2f7806cc955894cd33055197d54d05d56ceac
U-LAPTOP-ER85SC8R\tyama <tyama@preferred.jp>
2019-02-24 12:47:46 +0900

    Updated.

--- b063/b063.py
+++ b063/b063.py
@@ -1,34 +1,47 @@
 x = int(input())
 
+table10r = [0, 1, 2, -2, -1, 0, 1, 2, -2, -1]
+table5r  = [0, 1, 0, 1, 0, 1, 0, -1, 0, -1]
+
 acc = 0
 
-for a, b in [(250, 500), (50, 100), (25, 50), (5, 10), (3, 5)]:
-  q = (x + a) // b
-  x = x - q*b
-  acc += abs(q)
-  #print(q)
+for i in range(4):
+  ####
+  #q = x // 10
+  q = x // 5
+  r = x % 10
+  r5 = x % 5
 
-acc += abs(x)
+  if 3 <= r5:
+    q += 1
+
+  #print(x, table10r[r])
+  acc += abs(table10r[r])
+  x = q
+
+  ####
+  #q = x // 10
+  if (i < 3):
+    q = x // 2
+    r = x % 10
+
+    if r == 7 or r == 9:
+      q += 1
+    #if 6 <= r:
+    #  q += 1
+    #if 9 <= r:
+    #  q += 5
+    #elif 7 <= r:
+    #  q += 4
+    #elif 6 <= r:
+    #  q += 3
+    #elif 4 <= r:
+    #  q += 2
+    #elif 2 <= r:
+    #  q += 1
+    acc += abs(table5r[r])
+    #print(x, table5r[r])
+    x = q
+
 
 print(acc)
-
-#q = (x + 250) // 500
-#x = x - q*500
-#acc += q
-
-#q = (x + 50) // 100
-#x = x - q*100
-#acc += q
-
-#q = (x + 25) // 50
-#x = x - q*50
-#acc += q
-
-#q = (x + 5) // 10
-#x = x - q*10
-#acc += q
-
-#q = (x + 3) // 5
-#x = x - q*5
-#acc += q
-
