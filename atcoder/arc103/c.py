99fe5fdf7e5f464ea226f830b6856e96f97d073f
U-LAPTOP-ER85SC8R\tyama <tyama@preferred.jp>
2019-02-24 10:33:03 +0900

    First checkin.

---
+++ atcoder/arc103/c.py
@@ -0,0 +1,44 @@
+n = int(input())
+v = [int(x) for x in input().split()]
+
+de = {}
+do = {}
+
+for i in range(0, len(v), 2):
+  if v[i] in de:
+    de[v[i]] += 1
+  else:
+    de[v[i]] = 1
+  if v[i+1] in do:
+    do[v[i+1]] += 1
+  else:
+    do[v[i+1]] = 1
+
+de_k, do_k = \
+ [sorted(d.keys(), reverse=True, key=lambda k: d[k]) for d in [de, do]]
+
+print(de_k)
+print(do_k)
+
+if de[de_k[0]] < do[do_k[0]]:
+  dl   = do
+  dl_k = do_k
+  ds   = de
+  ds_k = de_k
+else:
+  dl   = de
+  dl_k = de_k
+  ds   = do
+  ds_k = do_k
+
+#print(n//2 - dl[dl_k[0]] - ds[ds_k[1 if dl_k[0] == ds_k[0] else 0]])
+n_dl = n//2 - dl[dl_k[0]]
+
+if dl_k[0] != ds_k[0]:
+  n_ds = ds_k[0]
+elif len(ds_k) <= 1:
+  n_ds = ds_k[0]
+else:
+  n_ds = n//2 - ds_k[1]
+
+print(n_dl + n_ds)
