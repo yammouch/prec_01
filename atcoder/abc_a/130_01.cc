99501b6ab26fd6db42dfe3eb82f6b27d1eb2173d
Takuya Yamauchi <tyama@preferred.jp>
2019-10-24 22:47:56 +0900

    First checkin.

---
+++ atcoder/abc_a/130_01.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int x, a;
+  cin >> x >> a;
+
+  if (x < a) {
+    cout << 0 << endl;
+  } else {
+    cout << 10 << endl;
+  }
+
+  return 0;
+}
