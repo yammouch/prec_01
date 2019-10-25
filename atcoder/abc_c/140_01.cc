123027cb5dcb6e70f976b24b785e2ff44469da4e
Takuya Yamauchi <tyama@preferred.jp>
2019-10-25 10:37:24 +0900

    First checkin.

---
+++ atcoder/abc_c/140_01.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int n, sum, b1;
+
+  cin >> n;
+  cin >> b1;
+  sum = b1;
+  for (int i = 0; i < n-2; i++) {
+    int b2;
+    cin >> b2;
+    if (b1 < b2) {
+      sum += b1;
+    } else {
+      sum += b2;
+    }
+    b1 = b2;
+  }
+  if (2 < n) {
+    cin >> b1;
+    sum += b1;
+  }
+
+  cout << sum << endl;
+  return 0;
+}
