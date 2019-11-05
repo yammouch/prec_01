245c7e30207989c2ffa11cfa5abb098f830fbfca
Takuya Yamauchi <tyama@preferred.jp>
2019-11-05 10:47:38 +0900

    Updated.

--- atcoder/abc_b/127_01.cc
+++ atcoder/abc_b/127_01.cc
@@ -4,14 +4,11 @@ using namespace std;
 
 int main()
 {
-  int a, b;
-  cin >> a >> b;
-  if (13 <= a) {
-    cout << b << endl;
-  } else if (6 <= a) {
-    cout << (b >> 1) << endl;
-  } else {
-    cout << 0 << endl;
+  int64_t r, d, x;
+  cin >> r >> d >> x;
+  for (int i = 0; i < 10; i++) {
+    x = x*r - d;
+    cout << x << endl;
   }
   return 0;
 }
